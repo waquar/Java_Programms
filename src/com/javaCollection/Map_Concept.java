171575b5842d1348127715b57dd6e75c6a901d8b
waquar <waquar.alam92@gmail.com>
2020-01-31 11:58:25 +0530

    added map

--- src/com/javaCollection/Map_Concept.java
+++ src/com/javaCollection/Map_Concept.java
@@ -1,7 +1,9 @@
 package com.javaCollection;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 
 
@@ -18,12 +20,13 @@ public class Map_Concept {
 	map.put(3, "D");
 	map.put(4, "C");
 	map.put(4, "E");
+	System.out.println("---Hash map----");
 	System.out.println(map.get(4));
 	
 	for (Map.Entry<Integer, String> entry : map.entrySet()) {
 		int key = entry.getKey();
 		String value = entry.getValue();
-		System.out.println(key + " - "  + value);
+		//System.out.println(key + " - "  + value);
 	}
 	//one more way for running loop
 	System.out.println("----");
@@ -31,9 +34,31 @@ public class Map_Concept {
 		String value = map.get(key);
 		System.out.println("Key: " + key + ", value: " + value);
 	}
-
 	
 
+	Map<Integer, String> linkedmap = new LinkedHashMap<Integer, String>();
+	linkedmap.putAll(map);
+	System.out.println("---Linked hash map----");
+	System.out.println(linkedmap.get(2));
+	
+	for (Integer key : linkedmap.keySet()) {
+		String value = linkedmap.get(key);
+		System.out.println(key + ": " + value);
+	
+	}
+	
+	Map <Integer , String> treemap = new TreeMap<Integer, String>();
+	treemap.putAll(linkedmap);
+	treemap.put(5, "F");
+	treemap.remove(3);
+	System.out.println("---Tree map----");
+	System.out.println(treemap.get(2));
+	
+	for (Map.Entry<Integer, String> entry : treemap.entrySet()) {
+		int key = entry.getKey();
+		String value = entry.getValue();
+		System.out.println(key + ": " + value);
+	}
 	}
 
 }
